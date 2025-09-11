449d30ffd971060f9d3f8697cf2f71b17129f7f6
demonicall <eldorbekumrzoqov56@gmail.com>
2025-09-11 22:19:30 +0500

    link

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,13 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://blogsitedemonicall.onrender.com/"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "blogsitedemonicall.onrender.com",
+    "*"
+]
+
 
 
 # Application definition
